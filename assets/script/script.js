e8a333fb6cc2500247f5396c189f2ccb02080498
Deh_Tarifa <denner_tarifa@outlook.com>
2022-11-21 16:38:35 -0300

    Atualização
    Adicionado 2 tela após o login.
    Atualizando o validação de dados.

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,10 +4,10 @@ const InputEmailLogin = document.getElementById('input-login-email')
 const InputPasswordLogin = document.getElementById('input-login-password')
 
 function ValidarBtnlogin() {
-  if (InputEmailLogin.value == 'adimin' && InputPasswordLogin.value == '123') {
+  if (InputEmailLogin.value == 'admin' && InputPasswordLogin.value == '123') {
     window.location = 'option.html'
   } else {
-    alert(' Login: adimin@.com \n Senha: @tester98')
+    alert(' eu amo minha namorada')
   }
 }
 
