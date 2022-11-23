6493a8e7f4b97f1420ca9c2d20bb4261542ba8a7
Deh_Tarifa <denner_tarifa@outlook.com>
2022-11-23 19:06:35 -0300

    Atualização script/css
    validação de dados da tela de login
    criação da página entrada de produtos

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,13 +1,48 @@
 // Tela de login button - Validação de dados
 
-const InputEmailLogin = document.getElementById('input-login-email')
-const InputPasswordLogin = document.getElementById('input-login-password')
+const inputEmail = document.getElementById('input-login-email')
+const inputPassword = document.getElementById('input-login-password')
+
+const form = document.querySelector('#form-login')
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+
+  ValidarBtnlogin()
+})
 
 function ValidarBtnlogin() {
-  if (InputEmailLogin.value == 'adimin' && InputPasswordLogin.value == '123') {
-    window.location = 'option.html'
+  const emailvalue = inputEmail.value.trim()
+  const passwordvalue = inputPassword.value.trim()
+
+  const containerEmail = document.querySelector('#container-login-email')
+  const containerPassword = document.querySelector('#container-login-password')
+  const spanLoginEmail = document.querySelector('.span-login-email')
+  const spanLoginPassword = document.querySelector('.span-login-password')
+
+  if (emailvalue === '' && passwordvalue === '') {
+
+    containerEmail.classList.add('incorrect')
+    containerPassword.classList.add('incorrect')
+
+    spanLoginEmail.innerHTML = 'preencha os campos vazios'
+    spanLoginPassword.innerHTML = 'preencha os campos vazios'
+
+    return
+  } else if (emailvalue === 'adimim' && passwordvalue === '123') {
+
+    window.location = 'assets/html/option.html'
+
+    return
+
   } else {
-    alert(' senha incorreta ')
+
+    containerEmail.classList.add('incorrect')
+    containerPassword.classList.add('incorrect')
+
+    spanLoginEmail.innerHTML = 'senha ou email incorretos'
+    spanLoginPassword.innerHTML = 'senha ou email incorretos'
+    
   }
 }
 
