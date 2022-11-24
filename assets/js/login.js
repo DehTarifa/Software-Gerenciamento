e2eaae03390b2f2f720a730882367ec9c2dcf001
Deh_Tarifa <denner_tarifa@outlook.com>
2022-11-23 21:03:48 -0300

    Atualização de css/escrita

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,26 +1,35 @@
-// Tela de login button - Validação de dados
+  // Variavel tela de login, evento 'submit' dispara função.
+  //--------------------------------------------------------
 
-const inputEmail = document.getElementById('input-login-email')
-const inputPassword = document.getElementById('input-login-password')
+const input_login_email = document.getElementById('input-login-email')
+const input_login_password = document.getElementById('input-login-password')
 
-const form = document.querySelector('#form-login')
+const form_login = document.querySelector('#form-login')
 
-form.addEventListener('submit', e => {
+form_login.addEventListener('submit', e => {
   e.preventDefault()
 
   ValidarBtnlogin()
 })
 
+  // Tela de login button - Validação de dados
+  //--------------------------------------------------------
+
 function ValidarBtnlogin() {
-  const emailvalue = inputEmail.value.trim()
-  const passwordvalue = inputPassword.value.trim()
+  // Variavel da tela de login 
+  //--------------------------------------------------------
+  const loginEmailValue = input_login_email.value.trim()
+  const loginPasswordValue = input_login_password.value.trim()
 
   const containerEmail = document.querySelector('#container-login-email')
   const containerPassword = document.querySelector('#container-login-password')
+
   const spanLoginEmail = document.querySelector('.span-login-email')
   const spanLoginPassword = document.querySelector('.span-login-password')
 
-  if (emailvalue === '' && passwordvalue === '') {
+  // Validação dos inputs 
+  //--------------------------------------------------------
+  if (loginEmailValue === '' && loginPasswordValue === '') {
 
     containerEmail.classList.add('incorrect')
     containerPassword.classList.add('incorrect')
@@ -29,12 +38,11 @@ function ValidarBtnlogin() {
     spanLoginPassword.innerHTML = 'preencha os campos vazios'
 
     return
-  } else if (emailvalue === 'adimim' && passwordvalue === '123') {
+  } else if (loginEmailValue === 'adimim' && loginPasswordValue === '123') {
 
     window.location = 'assets/html/option.html'
 
     return
-
   } else {
 
     containerEmail.classList.add('incorrect')
@@ -42,8 +50,7 @@ function ValidarBtnlogin() {
 
     spanLoginEmail.innerHTML = 'senha ou email incorretos'
     spanLoginPassword.innerHTML = 'senha ou email incorretos'
-    
   }
 }
 
-// Tela de login button - Validação de dados
+
